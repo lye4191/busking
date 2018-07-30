2961f84879961f3d80739aa57f56cd9cedac6914
spectrum08 <jsizx0808>
2018-07-30 19:31:14 +0900

    sugi_new

--- app/src/main/java/com/example/wjdck/busking/MainActivity.java
+++ app/src/main/java/com/example/wjdck/busking/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //yuni
-        //Project yunieeeeee323141323313221
-
-        //123123123123
+        //sugisugi
     }
 }
