1edf666c2da35647bd1f57f439bc38badc12ffea
djstjr14 <djstjr14@naver.com>
2018-07-31 02:37:10 +0900

    firebase연동, addbusk UI생성

--- app/src/main/java/com/example/wjdck/busking/AddbuskActivity.java
+++ app/src/main/java/com/example/wjdck/busking/AddbuskActivity.java
@@ -1,11 +1,10 @@
 package com.example.wjdck.busking;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AddbuskActivity extends AppCompatActivity {
 
@@ -13,17 +12,16 @@ public class AddbuskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbusk);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btn_cancle = (Button)this.findViewById(R.id.cancle_btn);
+
+        btn_cancle.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onBackPressed();
             }
         });
+
     }
 
 }
