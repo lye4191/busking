65dcddba03dfd4443596f3e5741bd80543f70551
spectrum08 <jsizx0808>
2018-08-01 04:25:34 +0900

    suki!

--- app/src/main/java/com/example/wjdck/busking/DetailActivity.java
+++ app/src/main/java/com/example/wjdck/busking/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.wjdck.busking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    public void donationbutton(View v){
+        Intent intent = new Intent(DetailActivity.this, Duration.class);
+        startActivity(intent);
+
     }
 }
