217aad007a4ee96645424e2c2f5eaa9f8889ce10
djstjr14 <djstjr14@naver.com>
2018-07-31 19:15:11 +0900

    firebase에 데이터저장

---
+++ app/src/main/java/com/example/wjdck/busking/Busk.java
@@ -0,0 +1,32 @@
+package com.example.wjdck.busking;
+
+import android.media.Image;
+
+class Busk{
+    public String roomname; //이런식으로 데이터에 구분할수있는 값 넣어줘야댐 아~~~ 그 예를들어서 roomnum이런식으로 1 2 3 카웅ㄴㅌ잉해서 그 child(roomname) 이런식으로?ㅇㅇ
+    //근데 이거 이런방법도있음
+    public String name;
+    public String locate;
+    public String time;
+    public String description;
+    public String genre;
+    public Image image;
+
+    public Busk(String name, String locate, String time, String description, String genre){
+        this.name = name;
+        this.locate = locate;
+        this.time = time;
+        this.description = description;
+        this.genre = genre;
+    }
+
+    public Busk(String name, String locate, String time, String description, String genre, Image image){
+        this.name = name;
+        this.locate = locate;
+        this.time = time;
+        this.description = description;
+        this.genre = genre;
+        this.image = image;
+    }
+
+}
